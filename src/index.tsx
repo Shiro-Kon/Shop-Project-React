a6128ab9e12820f28753d3ed74587dbb85909a6a
Shiro <Shiro-Shiki.com>
2023-05-18 20:24:53 +0300

    add react component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//React Element
+// const title = React.createElement('hl', { id: 'title' }, 'Hello React')
+//console.log(title)
+
+//JSX (завжди один елемент)
+// const app = (
+//     <div>
+//         <h1 id="text">Hello App</h1>
+//         <p>
+//             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
+//             impedit adipisci tenetur minus corporis fugit assumenda sunt facere,
+//             distinctio cumque asperiores cupiditate quisquam sint doloribus,
+//             vitae ab voluptatum placeat obcaecati.
+//         </p>
+//     </div>
+// )
+
+//React Component (завжди приймає один елемент тому потрібно огорнути в div)
+const App = () => {
+    return (
+        <div>
+            <h1>Hello App component</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+                ducimus officia neque porro eveniet eligendi, delectus et sequi
+                inventore dolor eius rem quasi quo, ex maiores iure laudantium!
+                Error, iste?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic
+                quos officiis, impedit consequatur architecto nostrum
+                dignissimos quaerat doloribus est soluta dolorum iusto, dicta ex
+                tempore delectus illum totam aspernatur blanditiis.
+            </p>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
