2a2058c42ae544f6ee30cc650638fc5e5d94e585
Shiro <Shiro-Shiki.com>
2023-05-23 20:42:32 +0300

    export Header

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
-import Title from './Title'
-import { Content } from './Content' //іменований експорт
-
-//React Component (завжди приймає один елемент тому потрібно огорнути в div) a ще цей метот стрілкового запису
-const App = () => {
-    return (
-        <>
-            <Title title="App" />
-            <Title title="React" num={1} />
-            <Content
-                text1={
-                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione omnis debitis tempora repudiandae, voluptate reiciendis odit nostrum mollitia vero nobis doloremque officia magni delectus praesentium eum perspiciatis eligendi maxime.'
-                }
-                text2={
-                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione omnis debitis tempora repudiandae, voluptate reiciendis odit nostrum mollitia vero nobis doloremque officia magni delectus praesentium eum perspiciatis eligendi maxime.'
-                }
-            />
-        </>
-    )
-}
+import App from './Container/App/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
