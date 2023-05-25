c163cca1038a2b063c385bcd215244e7d86dd9ef
Shiro <Shiro-Shiki.com>
2023-05-25 20:51:25 +0300

    added Home and Main

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,11 +1,13 @@
 import Header from 'Container/Header/Header'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
+import Main from 'Container/Main/Main'
 const App = () => {
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header />
+            <Main />
         </StyledEngineProvider>
     )
 }
