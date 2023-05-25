5265a89b636b5efa56ea99d03f4ccd98be88e6cf
Shiro <Shiro-Shiki.com>
2023-05-25 20:39:01 +0300

    add folder CartHeder adn add file CartHeader

--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -6,12 +6,13 @@ import { Container } from '@mui/material'
 import Logo from 'components/Logo/Logo'
 import Menu from 'components/Menu/Menu'
 import './Header.scss'
+import CartHeader from 'components/CartHeader/CartHeader'
 
 type Props = {}
 const Header = (props: Props) => {
     return (
-        <Container >
-            <AppBar position="static" className='app-bar' sx={{backgroundColor: 'seagreen'}}>
+        <Container>
+            <AppBar position="static" className="app-bar">
                 <Toolbar>
                     <IconButton
                         size="large"
@@ -24,7 +25,7 @@ const Header = (props: Props) => {
                     </IconButton>
                     <Logo />
                     <Menu />
-                    
+                    <CartHeader />
                 </Toolbar>
             </AppBar>
         </Container>
