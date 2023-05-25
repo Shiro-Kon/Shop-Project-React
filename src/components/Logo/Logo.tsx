4a0b184208924c93942f5e2decf9440893a1adee
Shiro <Shiro-Shiki.com>
2023-05-25 20:06:30 +0300

    created the menu folder and file

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,19 @@
+
+import Typography from '@mui/material/Typography'
+import logo from 'assets/logo.svg'
+
+type Props = {}
+
+const Logo = (props: Props) => {
+  return (
+    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                       <img src={logo} alt='fake-shop logo' />
+                    </Typography>
+  )
+}
+
+export default Logo
